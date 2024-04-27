3e71be272e31dbd876195484d6adbb0da88d2e28
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-04-27 16:23:20 -0300

    database script

---
+++ database/createUser.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(1) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
