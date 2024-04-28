a0fbee60ece4bc3c5aff4b5e7238ab0c373a45fb
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-04-28 20:24:47 -0300

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-<<<<<<< HEAD
 import userRoutes from './routes/userRoutes';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -9,21 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-=======
-
-const app = express();
-app.use(bodyParser.json());
->>>>>>> main
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-<<<<<<< HEAD
 app.use('/user', userRoutes);
 
-=======
->>>>>>> main
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 })
