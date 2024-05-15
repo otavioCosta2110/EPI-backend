17326f44511760ff3be79178e9dacd5b7b342b6d
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-05-15 13:03:02 -0300

    routes better names

--- src/routes/threadRoutes.ts
+++ src/routes/threadRoutes.ts
@@ -4,8 +4,8 @@ import ThreadController from "../controllers/threadController";
 const router = Router();
 
 const threadController = new ThreadController();
-router.get("/threads", threadController.getThreads);
-router.post("/threads", threadController.createThread);
-router.delete("/threads", threadController.deleteThread);
+router.get("/get", threadController.getThreads);
+router.post("/create", threadController.createThread);
+router.delete("/delete", threadController.deleteThread);
 
 export default router;
