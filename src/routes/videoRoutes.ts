f2ef22f94df31fc23325547fcbeaf39b90d0582f
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-05-16 23:09:04 -0300

    feat: adiciona o video assistido a uma tabela user_videos

--- src/routes/videoRoutes.ts
+++ src/routes/videoRoutes.ts
@@ -7,5 +7,6 @@ const videoController = new VideoController();
 router.get("/getvideos", videoController.getVideos);
 router.post("/create", videoController.createVideo);
 router.post("/delete", videoController.deleteVideo);
+router.post("/play", videoController.playVideo);
 
 export default router;
