e27fcad6e37cf90a085dd3ceab57b2dda86deb51
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-05-29 12:24:02 -0300

    feat: CRUD materials com upload de arquivos

---
+++ src/controllers/materialController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import MaterialServices from "../services/materialServices";
+
+export default class MaterialController {
+  materialServices = new MaterialServices();
+
+  constructor() {}
+  createMaterial = async (req: Request, res: Response) => {
+    try {
+      const { title, type, description } = req.body;
+      const fileUrl = req.file ? req.file.filename : undefined;
+      const materialData = { title, type, description, fileUrl };
+      const createdMaterial = await this.materialServices.createMaterial(
+        materialData
+      );
+      res.status(201).json({ data: createdMaterial });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  getMaterialById = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const material = await this.materialServices.getMaterialById(id);
+      if (!material) {
+        return res.status(404).json({ error: "Material not found" });
+      }
+      const responseData: any = { data: material };
+      if (material.file_url) {
+        responseData.data.file_url = material.file_url;
+      }
+      res.status(200).json(responseData);
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  updateMaterial = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const materialData = req.body;
+      const updatedMaterial = await this.materialServices.updateMaterial(
+        id,
+        materialData
+      );
+      res.status(200).json({ data: updatedMaterial });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  deleteMaterial = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      await this.materialServices.deleteMaterial(id);
+      res.status(204).end();
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+}
---
+++ src/repositories/materialRepositories.ts
@@ -0,0 +1,73 @@
+import pool from "../database";
+import MaterialModel from "../models/materialModel";
+
+export default class MaterialRepository {
+  constructor() {}
+
+  async createMaterial(material: MaterialModel): Promise<MaterialModel> {
+    const { id, title, type, description, file_url } = material;
+
+    const result = await pool.query(
+      "INSERT INTO materials (id, title, type, description, file_url) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [id, title, type, description, file_url]
+    );
+
+    const createdMaterialRow = result.rows[0];
+    const createdMaterial: MaterialModel = {
+      id: createdMaterialRow.id,
+      title: createdMaterialRow.title,
+      type: createdMaterialRow.type,
+      description: createdMaterialRow.description,
+      file_url: createdMaterialRow.file_url,
+    };
+
+    return createdMaterial;
+  }
+
+  async getMaterialById(id: string): Promise<MaterialModel | null> {
+    try {
+      const result = await pool.query("SELECT * FROM materials WHERE id = $1", [
+        id,
+      ]);
+      if (result.rows.length === 0) {
+        return null;
+      }
+      const materialRow = result.rows[0];
+      console.log("Material Row:", materialRow);
+      const material: MaterialModel = {
+        id: materialRow.id,
+        title: materialRow.title,
+        type: materialRow.type,
+        description: materialRow.description,
+        file_url: materialRow.file_url,
+      };
+      console.log("Material:", material);
+      return material;
+    } catch (error) {
+      console.error("Error fetching material:", error);
+      return null;
+    }
+  }
+
+  async updateMaterial(material: MaterialModel): Promise<MaterialModel> {
+    const result = await pool.query(
+      "UPDATE materials SET title = $1, type = $2, file_url = $3, description = $4 WHERE id = $5 RETURNING *",
+      [material.title, material.type, material.description, material.id]
+    );
+    const updatedMaterialRow = result.rows[0];
+    if (!updatedMaterialRow) {
+      throw new Error("Material not found");
+    }
+    const updatedMaterial: MaterialModel = {
+      id: updatedMaterialRow.id,
+      title: updatedMaterialRow.title,
+      type: updatedMaterialRow.type,
+      description: updatedMaterialRow.description,
+    };
+    return updatedMaterial;
+  }
+
+  async deleteMaterial(id: string): Promise<void> {
+    await pool.query("DELETE FROM materials WHERE id = $1", [id]);
+  }
+}
