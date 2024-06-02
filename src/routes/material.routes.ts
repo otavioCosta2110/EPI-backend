0e2ad3ffd0912ec748b873b1ac8080973383b56f
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-02 11:07:06 -0300

    teste

--- src/routes/material.routes.ts
+++ src/routes/material.routes.ts
@@ -28,6 +28,19 @@ router.post(
 router.get("/getmaterial/:id", materialController.getMaterialById);
 router.put("/updatematerial/:id", materialController.updateMaterial);
 router.delete("/deletematerial/:id", materialController.deleteMaterial);
+router.get("/download/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, "../uploads", filename);
+
+  res.download(filePath, (err) => {
+    if (err) {
+      res.status(404).send({
+        message: "File not found.",
+        error: err.message,
+      });
+    }
+  });
+});
 
 router.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
