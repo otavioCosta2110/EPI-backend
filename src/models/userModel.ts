c14f4887cc529c1b06c59c27625e85017f179740
Luiz Eduardo Picoli <leduardo3011@gmail.com>
2024-06-20 10:22:44 -0300

    feat: add image_url to user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,10 +1,11 @@
 export default class UserModel {
   constructor(
-   public id: string,
-   public name: string,
-   public email: string,
-   public password: string,
-   public role:string,
-   public tags:string[]
-  ){}
+    public id: string,
+    public name: string,
+    public email: string,
+    public password: string,
+    public role: string,
+    public tags: string[],
+    public image_url?: string | null
+  ) {}
 }
